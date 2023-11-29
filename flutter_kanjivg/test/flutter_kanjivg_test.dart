785bcba45e9c49a7b838c25f0d02b1693cea98ce
Nikodem Bernat <n.bernat@outlook.com>
2023-11-29 01:32:07 +0100

    Create basic project structure

--- flutter_kanjivg/test/flutter_kanjivg_test.dart
+++ flutter_kanjivg/test/flutter_kanjivg_test.dart
@@ -1,6 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_kanjivg/flutter_kanjivg.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
